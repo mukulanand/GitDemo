f52d575e64b781f9a3b7606aacfc3fcb8c287786
Mukul <mamukulanand@gmail.com>
2020-05-23 11:23:46 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	
+	public void androidApps(){
+		
+		System.out.println("androidAps");
+	}
+	
+	@Test
+	public void iosApps(){
+		System.out.println("iosApps");
+		
+	}
+}
+
+
